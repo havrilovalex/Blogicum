79029e47738a3a585d8cfdc035b14b01c5bdb322
Гаврилов Александр <havrilovalex02@gmail.com>
2025-02-02 20:53:20 +0300

    Внесены правки второй итерации и flake8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.mixins import (
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from django.utils.timezone import now
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView)
 
